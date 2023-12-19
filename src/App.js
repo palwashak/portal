5df2b3615209d591f98c9f9de6594e95f7e8d9ab
RIB\palwasha.khan <palwasha.khan@rib-international.com>
2023-12-19 15:02:02 +1000

    added a hello.js file to check routes

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import { useRoutes } from "react-router-dom";
 import { About } from "./screens/About";
 import NotificationsScreen from "./screens/NotificationsScreen";
 import { NotificationDetail } from "./screens/NotificationDetail";
+import Hello from "./screens/Hello.js";
 
 function App() {
   useEffect(() => {
     document.title = "Salesforce Service Cloud";
   }, []);
-  <h1>Welcome</h1>
+  <h1>Welcome</h1>;
   let element = useRoutes([
-    { path: "/", element: <About /> },
+    { path: "/", element: <Hello /> },
     { path: "/about", element: <About /> },
     { path: "/notifications", element: <NotificationsScreen /> },
     { path: "/notification/detail/:id", element: <NotificationDetail /> },
